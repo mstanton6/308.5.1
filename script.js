9ddceb3416baccc81d050061a8b81ea7a27442cb
mstanton6 <mstanton6@yahoo.com>
2025-12-10 17:23:13 -0500

    Part 2 done

--- script.js
+++ script.js
@@ -17,3 +17,22 @@ function sum_numbers() {
     console.log('Sum of the number is: ' + sum);
 }
 
+// Part 2
+//Take an array of numbers and return the average.
+
+let numbers2 = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+avg_numbers(numbers2);
+
+function avg_numbers() {
+    let sum = 0;
+    let avg = 0;
+    for (i = 0; i < numbers.length; i++) {
+
+        sum = sum + numbers[i];
+
+    }
+    avg = sum / numbers.length;
+    console.log('Part 2');
+    console.log('The average of the numbers is: ' + avg);
+}
